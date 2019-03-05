944d4487ce9e89aee4a371a2b3777749e45ad99a
Allan Bendy <allan.bendy@gmail.com>
2019-03-05 20:09:17 +1100

    replace gulp with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,58 @@
-module.exports = {
-  devtool: 'source-map',
-  mode: 'development',
+const webpack = require('webpack');
+const path = require('path');
+
+const base = {
+  entry: {
+    background: './src/scripts/background.js',
+    content: './src/scripts/content.js',
+  },
+  output: {
+    path: path.join(__dirname, 'app/assets/scripts/'),
+    filename: '[name].js',
+  },
   module: {
     rules: [{
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'eslint-loader',
+      enforce: 'pre',
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'eslint-loader',
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      }
-    ],
+    }],
   },
 };
+
+const development = {
+  ...base,
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    ...base.module,
+  },
+};
+
+const production = {
+  ...base,
+  mode: 'production',
+  module: {
+    ...base.module,
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+  ],
+};
+
+// eslint-disable-next-line no-undef
+if (NODE_ENV === 'development') {
+  module.exports = development;
+} else {
+  module.exports = production;
+}
