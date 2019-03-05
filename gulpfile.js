903c16a52295c06849b28aae0e94f3b046bc618e
Allan Bendy <allan.bendy@gmail.com>
2019-03-05 17:29:15 +1100

    adds `gulp clean` task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const babel = require('gulp-babel');
+const del = require('del');
 const { dest, parallel, series, src } = require('gulp');
 const { pipeline } = require('readable-stream');
 
@@ -7,16 +8,19 @@ const dirs = {
   dest: 'app/assets/scripts',
 };
 
+const clean = cb => del(`${dirs.dest}/*.*`, cb);
+
 const scripts = () => pipeline(
   src(`${dirs.src}/*.js`),
   babel(),
   dest(`${dirs.dest}`),
 );
 
-const build = series(scripts);
+const build = series(clean, scripts);
 
 module.exports = {
   default: build,
   build,
+  clean,
   scripts,
 };
