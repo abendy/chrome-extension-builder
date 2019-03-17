874b51bc294f3fb85ebfae9757cb4453ce723560
Allan Bendy <allan.bendy@gmail.com>
2019-03-18 01:10:01 +1100

    save and restore single highlight

--- src/scripts/highlight.js
+++ src/scripts/highlight.js
@@ -10,12 +10,89 @@ if (!rangy.initialized) {
   rangy.init();
 }
 
-
 const highlighter = rangy.createHighlighter();
 const cookieName = 'rangySerializedSelection';
+const deserializeRegex = /^([^,]+),([^,{]+)(\{([^}]+)\})?$/;
+
+const removeHighlight = (el) => {
+  highlighter.removeHighlights(el);
+  storage.remove(cookieName);
+  console.log('Removed highlight', el);
+};
+
+const doHighlight = (selection) => {
+  const classApplier = rangy.createClassApplier('highlight');
+  highlighter.addClassApplier(classApplier);
+  highlighter.highlightSelection('highlight', selection);
+
+  const highlight = highlighter.highlights[highlighter.highlights.length - 1];
+  // eslint-disable-next-line max-len
+  const highlightElements = highlighter.highlights[highlighter.highlights.length - 1].getHighlightElements();
+
+  highlightElements.forEach((el) => {
+    el.addEventListener('click', () => {
+      removeHighlight([highlight]);
+    });
+  });
+};
+
+function deserializePosition(serialized, rootNode, doc) {
+  const parts = serialized.split(':');
+  let node = rootNode;
+  const nodeIndices = parts[0] ? parts[0].split('/') : [];
+  let i = nodeIndices.length;
+
+  // eslint-disable-next-line no-plusplus
+  while (i--) {
+    const nodeIndex = parseInt(nodeIndices[i], 10);
+    if (nodeIndex < node.childNodes.length) {
+      node = node.childNodes[nodeIndex];
+    } else {
+      throw module.createError(`deserializePosition() failed': node ${rangy.dom.inspectNode(node)} has no child with index ${nodeIndex}, ${i}`);
+    }
+  }
+
+  return new rangy.dom.DomPosition(node, parseInt(parts[1], 10));
+}
+
+function deserializeRange(serialized, rootNode, doc) {
+  const result = deserializeRegex.exec(serialized);
+
+  const start = deserializePosition(result[1], rootNode, doc);
+  const end = deserializePosition(result[2], rootNode, doc);
+  const range = rangy.createRange(doc);
+  range.setStartAndEnd(start.node, start.offset, end.node, end.offset);
+  return range;
+}
+
+function deserializeSelection(serialized, rootNode, win) {
+  // eslint-disable-next-line no-param-reassign
+  rootNode = window.document.documentElement;
+
+  const serializedRanges = serialized.split('|');
+  const sel = rangy.getSelection(window);
+  const ranges = [];
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0, len = serializedRanges.length; i < len; ++i) {
+    ranges[i] = deserializeRange(serializedRanges[i], rootNode, window.document);
+  }
+  sel.setRanges(ranges);
+
+  return sel;
+}
 
 const restoreHighlight = () => {
   try {
+    const cookieData = Cookies.get(cookieName);
+    console.log(cookieData);
+    const selection = deserializeSelection(cookieData);
+
+    // Highlighter
+    doHighlight(selection);
+
+    // Deselect
+    selection.collapseToEnd();
   } catch (e) {
     console.log('ERROR', e);
   }
@@ -28,34 +105,6 @@ if (document.readyState !== 'loading') {
     restoreHighlight();
   });
 }
-
-const removeHighlight = (el) => {
-  highlighter.removeHighlights(el);
-  // classApplier.undoToRange(range);
-  storage.remove(cookieName);
-  console.log('Removed highlight', el);
-};
-
-const doHighlight = (selection) => {
-  const classApplier = rangy.createClassApplier('highlight');
-  highlighter.addClassApplier(classApplier);
-  highlighter.highlightSelection('highlight', selection);
-
-  // classApplier.applyToSelection();
-  // classApplier.applyToRange(range);
-
-  const highlight = highlighter.highlights[highlighter.highlights.length - 1];
-  // eslint-disable-next-line max-len
-  const highlightElements = highlighter.highlights[highlighter.highlights.length - 1].getHighlightElements();
-
-  highlightElements.forEach((el) => {
-    // console.log(highlighter.getHighlightForElement(el));
-
-    el.addEventListener('click', () => {
-      removeHighlight([highlight]);
-    });
-  });
-};
 
 const createHighlight = (e, throttle = false) => {
   // Detect double & triple mouse click
@@ -70,58 +119,24 @@ const createHighlight = (e, throttle = false) => {
   if (selection.toString().length === 0 || selection.isCollapsed) {
     return;
   }
-  const selectionStr = selection.toString();
 
   // Get range objects
   const ranges = selection.getAllRanges();
   const range = selection.rangeCount ? selection.getRangeAt(0) : null;
-  const rangeStr = range.toString();
-  const rangeHtml = range.toHtml();
 
+  // Save selection
+  let serializedRanges = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0, len = ranges.length; i < len; ++i) {
+    const rootNode = rangy.DomRange.getRangeDocument(range).documentElement;
+    const serialized = `${rangy.serializePosition(ranges[i].startContainer, ranges[i].startOffset, rootNode)},${rangy.serializePosition(ranges[i].endContainer, ranges[i].endOffset, rootNode)}`;
+    serializedRanges[i] = serialized;
+  }
+  serializedRanges = serializedRanges.join('|');
+  Cookies.set(cookieName, serializedRanges);
 
   // Highlighter
   doHighlight(selection);
-
-
-  // Save selection
-  const saved = rangy.saveSelection();
-  // console.log('savedSel', saved);
-
-  // const saved = rangy.saveRange(range);
-  // console.log('savedRange', saved);
-
-  const storeJson = JSON.stringify(saved);
-  storage.set({ [cookieName]: storeJson });
-
-  setTimeout(() => {
-    // Restore from original selection
-
-    // rangy.restoreSelection(saved, true);
-
-    // highlighter.highlightSelection('highlight');
-    // const reSelection = rangy.getSelection();
-    // reSelection.collapseToEnd();
-
-
-    // Restore from stored data
-
-    storage.get(cookieName, (resp) => {
-      const { [cookieName]: respJson } = resp;
-      const respObj = JSON.parse(respJson);
-
-      Object.keys(respObj.rangeInfos).forEach((key) => {
-        if (Object.prototype.hasOwnProperty.call(respObj.rangeInfos[key], 'document')) {
-          respObj.rangeInfos[key].document = window.document;
-        }
-      });
-
-      rangy.restoreSelection(respObj, true);
-      highlighter.highlightSelection('highlight');
-      const restoredSelection = rangy.getSelection();
-      highlighter.highlightSelection('highlight', restoredSelection);
-      selection.collapseToEnd();
-    });
-  }, 3000);
 
   // Deselect
   selection.collapseToEnd();
