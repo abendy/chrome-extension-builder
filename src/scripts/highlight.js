c3a5f2bd13f61d18e8a5718974d3f31337921d12
Allan Bendy <allan.bendy@gmail.com>
2019-03-21 18:40:57 +1100

    adds dotenv and boilerplate env file

--- src/scripts/highlight.js
+++ src/scripts/highlight.js
@@ -3,6 +3,7 @@ import { rangyHighlight as Highlight } from 'rangy-updated/lib/rangy-highlighter
 import { rangyClassApplier as ClassApplier } from 'rangy-updated/lib/rangy-classapplier';
 import { rangySerializer as Serializer } from 'rangy-updated/lib/rangy-serializer';
 import { rangySelectionsaverestore as Saver } from 'rangy-updated/lib/rangy-selectionsaverestore';
+import config from 'dotenv';
 import api from 'axios';
 import Cookies from 'js-cookie';
 import { deserializeSelection } from './utils/highlight-utils';
@@ -14,6 +15,7 @@ class Highlighter {
     this.rangy.init();
 
     this.highlighter = this.rangy.createHighlighter();
+    this.db_host = process.env.DB_HOST;
 
     this.selection = null;
     this.ranges = null;
